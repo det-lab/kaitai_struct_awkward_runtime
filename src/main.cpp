31afbc52f49f927e49ceb2f02809f87251bc0844
Manasvi Goyal <mg.manasvi@gmail.com>
2023-09-25 14:32:10 +0200

    Update pybind11 with example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include "awkward/LayoutBuilder.h"
 #include <fstream>
 #include <iostream>
+#include "../build/_deps/awkward-headers-src/layout-builder/awkward/LayoutBuilder.h"
 #include "../src-animal/animal.h"
 
 namespace py = pybind11;
+
+// Defines taken from how-to-use-header-only-layoutbuilder.md
+using UserDefinedMap = std::map<std::size_t, std::string>;
+template<class... BUILDERS>
+using RecordBuilder = awkward::LayoutBuilder::Record<UserDefinedMap, BUILDERS...>;
+template<std::size_t field_name, class BUILDER>
+using RecordField = awkward::LayoutBuilder::Field<field_name, BUILDER>;
+template<class PRIMITIVE, class BUILDER>
+using ListOffsetBuilder = awkward::LayoutBuilder::ListOffset<PRIMITIVE, BUILDER>;
+template<class PRIMITIVE>
+using NumpyBuilder = awkward::LayoutBuilder::Numpy<PRIMITIVE>;
+enum Field : std::size_t {
+    one, two
+};
+
 
 /**
  * Create a snapshot of the given builder, and return an `ak.Array` pyobject
@@ -50,20 +65,55 @@ py::object snapshot_builder(const T &builder) {
         );
     }
     return from_buffers(builder.form(), builder.length(), container);
+
 }
 
+
 /**
- * Create demo array, and return its snapshot
+ * Create array, and return its snapshot
  * @return pyobject of Awkward Array
  */
-py::object create_awkward_array(const std::string& binary_data) {
-    std::ifstream infile(binary_data, std::ifstream::binary);
-    kaitai::kstream ks(&infile);
-    animal_t zoo = animal_t(&ks);
+py::object create_awkward_array() {
+    UserDefinedMap fields_map({
+        {Field::one, "one"},
+        {Field::two, "two"}
+    });
+
+    RecordBuilder<
+            RecordField<Field::one, NumpyBuilder<double>>,
+            RecordField<Field::two, ListOffsetBuilder<int64_t,
+                    NumpyBuilder<int32_t>>>
+    > builder(fields_map);
+
+    auto &one_builder = builder.content<Field::one>();
+    auto &two_builder = builder.content<Field::two>();
+
+    one_builder.append(1.1);
+
+    auto &two_subbuilder = two_builder.begin_list();
+    two_subbuilder.append(1);
+    two_builder.end_list();
+
+    one_builder.append(2.2);
+
+    two_builder.begin_list();
+    two_subbuilder.append(1);
+    two_subbuilder.append(2);
+    two_builder.end_list();
+
+    one_builder.append(3.3);
+    size_t data_size = 3;
+    int32_t data[3] = {1, 2, 3};
+
+    two_builder.begin_list();
+    two_subbuilder.extend(data, data_size);
+    two_builder.end_list();
+
     return snapshot_builder(builder);
 }
 
-PYBIND11_MODULE(_core, m) {
-    m.doc() = "pybind11 for generating awkward arrays";
+
+PYBIND11_MODULE(awkward_kaitai, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
     m.def("create_awkward_array", &create_awkward_array, "A function that creates an awkward array");
 }
