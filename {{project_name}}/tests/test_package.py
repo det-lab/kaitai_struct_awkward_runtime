cd8153781d8efd8709e6b0cec35ae74f636d3816
Angus Hollands <goosey15@gmail.com>
2023-10-09 14:39:27 +0100

    wip: more copier

--- {{project_name}}/tests/test_package.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import importlib.metadata
-
-import kaitai_awkward_runtime as m
-
-
-def test_version():
-    assert importlib.metadata.version("kaitai_awkward_runtime") == m.__version__
