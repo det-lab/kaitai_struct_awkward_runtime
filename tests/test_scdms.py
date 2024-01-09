1f433518ab11bff762b7d4026e86305db05ec015
Andrea Zonca <code@andreazonca.com>
2024-01-09 23:37:12 +0000

    test: test scdms.ksy

---
+++ tests/test_scdms.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import sys
+
+import numpy as np
+
+sys.path.append("local")
+import awkward_kaitai
+
+import json
+import gzip
+
+
+def test_scdms():
+    reader = awkward_kaitai.Reader("test_artifacts/libscdms.so")
+    awkward_array = reader.load("example_data/data/scdms_v_two_trigger.bin")
+
+    with gzip.open("tests/scdms.json.gz", "rb") as f:
+        expected_content = json.load(f)
+
+    assert awkward_array.to_list() == expected_content
