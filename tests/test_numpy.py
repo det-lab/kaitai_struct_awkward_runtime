7d0e8c9e009284ccaf3ef809e77af848f41c1fdb
Andrea Zonca <code@andreazonca.com>
2024-01-09 20:20:24 +0000

    test: testing numpy.ksy

---
+++ tests/test_numpy.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import sys
+
+import numpy as np
+
+sys.path.append("local")
+import awkward_kaitai
+
+
+def test_numpy():
+    reader = awkward_kaitai.Reader("test_artifacts/libnumpy.so")
+    awkward_array = reader.load("example_data/data/numpy.raw")
+
+    expected_array = np.array(
+            [8], dtype=np.uint32
+    )
+
+    np.testing.assert_array_equal(
+        awkward_array.numpyA__Znumpy_array.to_numpy(), expected_array
+    )
