91b01bcc8adbf6f6e648d34bcb2814de40097e9e
Andrea Zonca <code@andreazonca.com>
2025-05-07 00:22:14 +0000

    adapt debug_tests to soudan

--- tests/debug_tests.cpp
+++ tests/debug_tests.cpp
@@ -1,11 +1,11 @@
 // Select the test case by choosing the header and the test file
-#include "scdms.h"
+#include "soudan.h"
 #define TESTFILE "../example_data/data/minimal_sample.soudan"
 
 int main()
 {
     std::string file_path = TESTFILE;
-    ScdmsBuilderType* builder = load(file_path);
+    SoudanBuilderType* builder = load(file_path);
     std::cout << builder->form() << std::endl;
     std::cout << "Execution completed" << std::endl;
     std::size_t index = 0;
